28fd5e84d809b3a8d1c898876680557e1162e12e
JeongByungJoo <cheng286@naver.com>
2024-01-21 12:24:42 +0900

    [feat] 예약한 주문 구매 패키지 중departureDate를 기준으로 가까운 날짜를 조회를 위한 관련 메서드들 추가 _ userOrderWithEarliestDepartureDate, departureDateForOrder, getPackageDepartureOption

--- src/main/java/com/yanolja_final/domain/order/service/OrderService.java
+++ src/main/java/com/yanolja_final/domain/order/service/OrderService.java
@@ -7,8 +7,14 @@ import com.yanolja_final.domain.order.exception.OrderNotFoundException;
 import com.yanolja_final.domain.order.repository.OrderRepository;
 import com.yanolja_final.domain.packages.entity.Package;
 import com.yanolja_final.domain.packages.entity.PackageDepartureOption;
+import com.yanolja_final.domain.packages.exception.PackageDateNotFoundException;
+import com.yanolja_final.domain.packages.repository.PackageDepartureOptionRepository;
 import com.yanolja_final.domain.user.entity.User;
+import com.yanolja_final.domain.user.exception.UserNotFoundException;
+import com.yanolja_final.domain.user.repository.UserRepository;
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +27,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final PackageDepartureOptionRepository packageDepartureOptionRepository;
+    private final UserRepository userRepository;
 
     public Page<Order> read(User user, Pageable pageable) {
         return orderRepository.findByUserId(user.getId(), pageable);
@@ -50,5 +58,34 @@ public class OrderService {
         String orderCode = String.format("%04d%02d%02d%05d",
             today.getYear(), today.getMonthValue(), today.getDayOfMonth(), todayOrderCount);
         return orderCode;
+    }
+
+    public Order userOrderWithEarliestDeparture(Long id) {
+        User user = userRepository.findById(id)
+            .orElseThrow(UserNotFoundException::new);
+        List<Order> orders = user.getOrders();
+
+        if (orders.isEmpty()) {
+            throw new OrderNotFoundException();
+        }
+        Order userOrderWithEarliestDeparture = orders.stream()
+            .min(Comparator.comparing(o -> departureDateForOrder(o)))
+            .orElseThrow(OrderNotFoundException::new);
+        return userOrderWithEarliestDeparture;
+    }
+
+    private LocalDate departureDateForOrder(Order order) {
+        Long availableDateId = order.getAvailableDateId();
+        PackageDepartureOption departureOption = packageDepartureOptionRepository.findById(availableDateId)
+            .orElseThrow(PackageDateNotFoundException::new);
+        LocalDate departureDate = departureOption.getDepartureDate();
+        return departureDate;
+    }
+
+    public PackageDepartureOption getPackageDepartureOption(Order order) {
+        Long availableDateId = order.getAvailableDateId();
+        PackageDepartureOption departureOption = packageDepartureOptionRepository.findById(availableDateId)
+            .orElseThrow(PackageDateNotFoundException::new);
+        return departureOption;
     }
 }
