7c2cc4bbb9da79a9a5c8b448351393bbd1ed784d
JeongByungJoo <cheng286@naver.com>
2024-01-21 12:26:21 +0900

    [feat] 다가오는 패키지 조회 메서드 추가 _ getUpcomingPackage

--- src/main/java/com/yanolja_final/domain/user/facade/MyPageFacade.java
+++ src/main/java/com/yanolja_final/domain/user/facade/MyPageFacade.java
@@ -1,8 +1,14 @@
 package com.yanolja_final.domain.user.facade;
 
+import com.yanolja_final.domain.order.entity.Order;
+import com.yanolja_final.domain.order.service.OrderService;
+import com.yanolja_final.domain.packages.entity.PackageDepartureOption;
+import com.yanolja_final.domain.packages.service.PackageService;
 import com.yanolja_final.domain.user.dto.request.UpdateMyPageRequest;
 import com.yanolja_final.domain.user.dto.request.UpdatePasswordRequest;
 import com.yanolja_final.domain.user.dto.response.MyPageResponse;
+import com.yanolja_final.domain.user.dto.response.UpcomingPackageResponse;
+import com.yanolja_final.domain.user.entity.User;
 import com.yanolja_final.domain.user.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class MyPageFacade {
 
     private final MyPageService myPageService;
+    private final OrderService orderService;
+    private final PackageService packageService;
 
     public MyPageResponse updateUserInfo(UpdateMyPageRequest request, Long userId) {
         MyPageResponse response = myPageService.updateUserInfo(request, userId);
@@ -25,5 +33,24 @@ public class MyPageFacade {
 
     public void updatePassword(UpdatePasswordRequest request, Long userId) {
         myPageService.updatePassword(request, userId);
+    }
+
+    public UpcomingPackageResponse getUpcomingPackage(Long userId) {
+        User user = userId == null ? null : myPageService.findById(userId);
+        Order userOrder = orderService.userOrderWithEarliestDeparture(userId);
+        PackageDepartureOption departureOption = orderService.getPackageDepartureOption(userOrder);
+        Long dday = packageService.calculateDday(departureOption);
+        String departureDate = packageService.formattedDepartureDate(departureOption);
+        String endDate = packageService.formattedEndDate(departureOption);
+
+        return new UpcomingPackageResponse(
+            userOrder.getAPackage().getId(),
+            userOrder.getAPackage().getThumbnailImageUrl(),
+            userOrder.getAPackage().getTitle(),
+            dday,
+            userOrder.getAPackage().getNationName(),
+            departureDate,
+            endDate
+            );
     }
 }
